7328631a089d9f6f0caa0b52e48e51614b500693
tills13 <tills13@gmail.com>
2014-02-26 11:32:28 -0800

    major update - move to current dbx api

--- java/com/jseb/worldsync/commands/WorldSyncCommand.java
+++ java/com/jseb/worldsync/commands/WorldSyncCommand.java
@@ -1,112 +1,162 @@
 package com.jseb.worldsync.commands;
 
 import com.jseb.worldsync.WorldSync;
-import com.jseb.worldsync.tasks.BackupTask;
+import com.jseb.worldsync.helpers.ZipHelper;
+import com.jseb.worldsync.tasks.SyncTask;
+import com.jseb.worldsync.tasks.DownloadTask;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.entity.Player;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
 
-import com.dropbox.client2.exception.DropboxException;
+import com.dropbox.core.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.Calendar;
-import java.util.List;
+import java.util.logging.Logger;
 
 public class WorldSyncCommand implements CommandExecutor {
+	private final static Logger LOGGER = Logger.getLogger("WorldSyncCommand");
 	private WorldSync plugin;
-    public BackupTask backupTask;
-    public List<BukkitTask> backups;
+	public SyncTask syncTask;
+	public Thread restoreTask;
 
 	public WorldSyncCommand(WorldSync plugin) {
 		this.plugin = plugin;
-        this.backupTask = null;
+		this.syncTask = null;
 	}
 
 	@Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    	if (args.length == 0) return true;
+	public boolean onCommand(final CommandSender sender, Command cmd, String label, final String[] args) {
+		if (args.length == 0) helpSyntax(sender);
+		else {
+			if (args[0].equalsIgnoreCase("link")) {
+				if (plugin.client != null) {
+					if (sender instanceof Player) {
+						sender.sendMessage("[WS] already authenticated/linked");
+						sender.sendMessage("[WS] to re-auth, delete \"accesstoken.yml\"");
+					} else  LOGGER.info("[WS] already authenticated, delete \"accesstoken.yml\" to re-auth");
+				} else {
+					if (plugin.webAuth == null) sender.sendMessage("[WS] you need to start the auth session");
+					else WorldSync.finishDropboxAuth(args[1], plugin.getDataFolder());
+				}
+			} else if (args[0].equalsIgnoreCase("backup")) { // create a backup
+				if (args.length == 2) ZipHelper.zip(Bukkit.getServer().getWorld(args[1]).getWorldFolder(), new File(plugin.getDataFolder() + File.separator + "backups" + File.separator + Bukkit.getServer().getWorld(args[1]).getName() + "_backup.zip"));
+				else for (World world : plugin.getServer().getWorlds()) ZipHelper.zip(world.getWorldFolder(), new File(plugin.getDataFolder() + File.separator + "backups" + File.separator + world.getName() + "_backup.zip"));
+			} else if (args[0].equalsIgnoreCase("sync")) { // sync to dropbox
+				if (this.restoreTask != null && this.restoreTask.isAlive()) sender.sendMessage("[WS] restore currently running, please wait until restore complete");
+				else {
+					this.syncTask = new SyncTask(plugin, args.length == 2 ? Bukkit.getServer().getWorld(args[1]) : ((Player) sender).getWorld(), true); // change to sync
+					plugin.getServer().getScheduler().runTaskAsynchronously(plugin, this.syncTask);
+				}
+			} else if (args[0].equalsIgnoreCase("cancel")) { // cancel current sync
+				if (this.syncTask != null && this.syncTask.isActive) syncTask.cancel();
+				else if (this.restoreTask != null && this.restoreTask.isAlive()) restoreTask.interrupt();
+				else {
+					if (sender instanceof Player) sender.sendMessage("[WS] sync/restore is not currently running");
+					else LOGGER.info("[WS] sync/restore is not currently running");
+				}	
+			} else if (args[0].equalsIgnoreCase("restore")) { // restore world from dropbox
+				if (this.syncTask != null && this.syncTask.isActive) sender.sendMessage("[WS] sync currently running, please wait until sync complete");
+				else {
+					final WorldSync mPlugin = plugin;
+					this.restoreTask = new Thread(new Runnable() {
+						public void run() {
+							World mWorld = Bukkit.getServer().getWorld(args[1]);
+							if (mWorld != null) {
+								try {
+									Bukkit.getServer().broadcastMessage("[WS] world restore in progress, stand by...");
+									Bukkit.getServer().unloadWorld(mWorld, false);
 
-        if (args[0].equalsIgnoreCase("link")) {
-            try {
-                if (plugin.accessToken != null) {
-                    System.out.println("[WS] already authenticated/linked");
-                    System.out.println("[WS] to re-auth, delete \"accesstoken.dat\"");
-                    return true;
-                }
-                if (plugin.was == null) return false;
+									LOGGER.info("[WS] downloading zip...");
+									new DownloadTask(plugin, args.length == 2 ? Bukkit.getServer().getWorld(args[1]) : ((Player) sender).getWorld(), true).run();
 
-                plugin.uid = plugin.was.retrieveWebAccessToken(plugin.info.requestTokenPair);
-                plugin.accessToken = plugin.was.getAccessTokenPair();
-            } catch (DropboxException e) {
-                System.out.println("[WS] error linking: " + e.getMessage());
-                return false;
-            }
-            System.out.println("[WS] successfully linked");
-        } else if (args[0].equalsIgnoreCase("backup")) {
-            if (args.length > 2) return false; //backup with delay
+									LOGGER.info("[WS] unzipping result...");
+									ZipHelper.unzip(new File(mPlugin.getDataFolder(), File.separator + "downloaded_backups" + File.separator + args[1] + "_backup.zip"), new File(mPlugin.getDataFolder(), File.separator + "downloaded_backups" + File.separator + args[1]));
 
-            if (this.backupTask != null) {
-                if (this.backupTask.isActive) {
-                    if (args.length == 2) {
-                        if (!args[1].equalsIgnoreCase("cancel")) {
-                            System.out.println("[WS] backup in progress");
-                            return true;
-                        } else {
-                            System.out.println("[WS] attempting to cancel...");
-                        }
-                    } else {
-                        System.out.println("[WS] backup in progress");
-                        return true;
-                    }
-                }
-            }
+									LOGGER.info("[WS] swapping world...");
 
-            if (args.length == 1) {
-                backupTask = new BackupTask(plugin);
-                plugin.getServer().getScheduler().runTaskAsynchronously(plugin, backupTask);
-                return true;
-            }
+									Path oldWorldFolder = FileSystems.getDefault().getPath(mPlugin.getDataFolder().getPath() + File.separator + "downloaded_backups" + File.separator + mWorld.getName());
+									Path newWorldFolder = FileSystems.getDefault().getPath(mWorld.getWorldFolder().getPath());
+							
+									Files.move(newWorldFolder, oldWorldFolder, java.nio.file.StandardCopyOption.ATOMIC_MOVE);
+									Bukkit.getServer().createWorld(new WorldCreator(mWorld.getName()));
+									LOGGER.info("[WS] swapped and reloaded world");
+								} catch (IOException e) {
 
-            if (args[1].equalsIgnoreCase("now")) {
-                backupTask = new BackupTask(plugin);
-                plugin.getServer().getScheduler().runTaskAsynchronously(plugin, backupTask);
-            } else if (args[1].equalsIgnoreCase("tonight")) { //backup at midnight
-                backupTask = new BackupTask(plugin);
-                Calendar cal = Calendar.getInstance();
-                int curHour = cal.get(Calendar.HOUR);
-                int delay = (cal.get(Calendar.AM_PM) == 0) ? 24 - curHour : 12 - curHour;
-                System.out.println("[WS] backup will begin in " + delay + " hours");
-                plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, backupTask, delay * 60 * 20 * 60);
-            } else if (args[1].equalsIgnoreCase("cancel")) {
-                if (backupTask.isActive) {
-                    backupTask.cancel();
-                } else {
-                    backupTask = null;
-                    System.out.println("[WS] backup is not currently running");
-                }
-            } else {
-                try {
-                    backupTask = new BackupTask(plugin);
-                    int delay = Integer.parseInt(args[1]);
-                    System.out.println("[WS] backup will begin in " + delay + " hours");
-                    plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, backupTask, delay * 20 * 60 * 60);
-                } catch (NumberFormatException e) {
-                    backupTask = null;
+								}
+							} else System.out.println("asdasd");
+						}
+					});
 
-                    System.out.println("[WS] unable to parse arguments");
-                    return true;
-                }
-            }
-        } else if (args[0].equalsIgnoreCase("restore")) {
-        } else if (args[0].equalsIgnoreCase("info")) {
-            sender.sendMessage("[WS] connection info: ");
-            sender.sendMessage("uid: " + (plugin.accessToken == null ? "not connected" : plugin.uid));
-            sender.sendMessage("key: " + (plugin.accessToken == null ? "not connected" : plugin.accessToken.key));
-            sender.sendMessage("secret: " + (plugin.accessToken == null ? "not connected" : plugin.accessToken.secret));
-            //sender.sendMessage(": " + plugin.accessToken == null ? "not connected" : plugin.accessToken.secret);
-        }
+					restoreTask.start();
+				}
+			} else if (args[0].equalsIgnoreCase("list")) { // list of remote and local backups
+				sender.sendMessage("[WS] local backups");
+				File backupDir = new File(plugin.getDataFolder(), "backups");
+				if (backupDir.exists()) {
+					for (File file : backupDir.listFiles()) sender.sendMessage("    " + file.getName());
+				} else sender.sendMessage("    no local backups; 'ws backup <world>' to perform a backup");
 
-        return true;
-    }
+				final WorldSync mPlugin = plugin;
+				new Thread(new Runnable() {
+					public void run() {
+						try {
+							if (mPlugin.client != null) {
+								DbxEntry.WithChildren listing = mPlugin.client.getMetadataWithChildren("/backups");
+								sender.sendMessage("[WS] remote backups");
+								if (listing != null) {
+									for (DbxEntry child : listing.children) sender.sendMessage("    " + child.name + " modified " + child.asFile().lastModified);	
+								} else sender.sendMessage("    no remote backups; 'ws sync <world>' to sync a backup");
+							} else sender.sendMessage("[WS] plugin needs to be linked to display remote backups");
+						} catch (DbxException e) {
+							sender.sendMessage("[WS] something went wrong while displaying remote backups");
+							LOGGER.info("[WS] something went wrong while displaying remote backups: " + e.getMessage());
+						}
+					}
+				}).start();
+			} else if (args[0].equalsIgnoreCase("session")) {
+				final WorldSync mPlugin = plugin;
+				new Thread(new Runnable() {
+					public void run() {
+						try {
+							if (mPlugin.client != null) {
+								DbxAccountInfo acctInfo = mPlugin.client.getAccountInfo();
+								sender.sendMessage("[WS] session info");
+								sender.sendMessage("    user: " + acctInfo.displayName);
+								sender.sendMessage("    user id: " + acctInfo.userId);
+								sender.sendMessage("    quota: " + acctInfo.quota);
+								sender.sendMessage("    token: " + mPlugin.client.getAccessToken());
+							} else sender.sendMessage("[WS] plugin needs to be linked to display remote backups");
+						} catch (DbxException e) {
+							sender.sendMessage("[WS] something went wrong while displaying remote backups");
+							LOGGER.info("[WS] something went wrong while displaying remote backups: " + e.getMessage());
+						}
+					}
+				}).start();
+			} else helpSyntax(sender);
+		}
+
+		return true;
+	}
+
+	public void helpSyntax(CommandSender sender) {
+		sender.sendMessage("[WS] command overview");
+		sender.sendMessage("    1. ws link <code> - link the app to dropbox");
+		sender.sendMessage("    2. ws backup <world> - create backup of specified world or all worlds");
+		sender.sendMessage("    3. ws sync <world> - sync world specified or current world");
+		sender.sendMessage("    4. ws cancel - cancels sync in progress");
+		sender.sendMessage("    5. ws restore - restores specified world or all worlds");
+		sender.sendMessage("    6. ws session - information about the current session");
+		sender.sendMessage("    7. ws list - list local and remote backups");
+	}
 }
